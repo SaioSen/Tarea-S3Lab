5c9a6c923684a27e5b92036391c9d51916e9eca3
Teresa Atencio <teresa.atencio.robles@hotmail.com>
2021-08-25 17:56:37 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tarea_S3Lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
