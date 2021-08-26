da6a8994700a6a5c3043a1b1d67dca827ead1d71
Teresa Atencio <teresa.atencio.robles@hotmail.com>
2021-08-25 20:05:43 -0500

    Entidad Alumno - Funciona el registrar en la bd

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Tarea_S3Lab.Models;
+using Tarea_S3Lab.Data;
 
 
 namespace Tarea_S3Lab.Controllers
 {
     public class AlumnoController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public AlumnoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
 
         public IActionResult Index()
         {
@@ -16,6 +25,8 @@ namespace Tarea_S3Lab.Controllers
         public IActionResult Crear(Alumno objAlumno)
         {
 
+            _context.Add(objAlumno);
+            _context.SaveChanges();
             ViewData["Message"] = "Alumno Registrado";
 
             return View("Index");
--- Models/Alumno.cs
+++ Models/Alumno.cs
@@ -1,19 +1,37 @@
 using System;
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace Tarea_S3Lab.Models
 {
 
+    [Table("t_alumno")]
+
+
+
     public class Alumno
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        [Column("id")]
+        public int Id { get; set; }
+
+
+        [Column("nombres")]
         public string Nombres { get; set;}
 
+        [Column("apellidos")]
         public string Apellidos { get; set;}
 
+        [Column("FechaNaci")]
         public DateTime Time { get; set;}
 
-        public string DNI { get; set;}
+        [Column("dni")]
+        public int DNI { get; set;}
 
+        [Column("genero")]
         public string Genero { get; set;}
+        [Column("carrera")]
 
         public string Carrera { get; set;}
     }
